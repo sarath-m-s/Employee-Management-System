8517b4b95903b26bc9b5928a1710e3a5f12ebc87
SARATH M S <sarathms789@gmail.com>
2024-08-26 21:54:21 +0530

    chore: Add routes and components for managing departments

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const HeaderComponent = () => {
   return (
     <div>
       <header>
-        <nav className="navbar navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <a href="/" className="navbar-brand">
-            Employee Management App
+            Employee Management System
           </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/employees">
+                  Employees
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/departments">
+                  Departments
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
     </div>
